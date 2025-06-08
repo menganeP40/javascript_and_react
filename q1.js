e02ea0377113d25fa1508348865c81e566f801d6
menganeP40 <menganeprashant40@gmail.com>
2025-06-08 19:53:18 +0530

    Initial commit

---
+++ q1.js
@@ -0,0 +1,116 @@
+//let a = 10;
+//let b = 20;
+//let c = 30;
+//console.log(a+b+c)
+
+//difference between declaring a variable with var and let is when you
+//let a = 10;
+//console.log(a)
+//output => 10
+//
+//but if we call it first and then declare , then it will show us error obviously 
+//
+///////////////////////////////////
+//but in the case of var 
+//
+//console.log(a)
+//var a = 10;
+//output => undefined
+//i mean it will not give an error 
+
+
+
+
+///////////////////////////////////
+
+//type cohersion 
+//"1" + 1 => "11"
+//"1" - 1 => 0
+//"1" * 1 => 1
+//"1" / 1 => 1
+
+///////////////////////////////////
+//Q: accept and print number 
+//let age = Number(prompt("what is your age?"));
+//console.log(age)
+//console.log(typeof age)
+
+
+///////////////////////////////////
+//Q: swap two variables via 3 methods 
+//method 1 
+//let a = 10;
+//let b = 20;
+//let c = a;
+//a = b;
+//b = c;
+//
+//console.log(a)                                                    shift V to select lines then : , normal I// (to comment out)
+//console.log(b)
+
+
+
+
+
+
+///////////////////////////////////
+//Q: sum of all natural numbers from 1 to n 
+
+//function sumOfNatNums(num){
+  //let sum = 0;
+  //for(let i=1; i<=num; i++){
+    //sum = sum + i
+//
+  //}
+  //return sum;
+//}
+//
+//console.log(sumOfNatNums(5))
+
+///////////////////////////////////
+//Q:kind of unpacking in js 
+//let a = 10;
+//let b = 20;
+//[a,b] = [b,a]
+//
+//console.log(a,b)
+
+///////////////////////////////////
+// difference between mod and floor 
+let a = 12;
+let b = 22;
+console.log(Math.floor(b/a)); //this will give me quetiant 
+console.log(b%a); // this will give me remainder 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
