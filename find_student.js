7fb383de80d48256d96b6521ab40d8e3b56de3d4
menganeP40 <menganeprashant40@gmail.com>
2025-06-09 10:26:38 +0530

    find student question

--- find_student.js
+++ find_student.js
@@ -1,6 +1,12 @@
 const studentDatabase = ['prashant','erick','amey','rishika'];
 
 const findStudent = (allStudent , student_name) => {
-  
+  for(let i=1; i<=allStudent.length; i++){
+    if(allStudent[i] === student_name){
+      console.log(`found ${student_name} at position ${i}`);
+    }
+  } 
 
 }
+
+findStudent(studentDatabase,"rishika")
