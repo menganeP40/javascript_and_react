6579adf0fc26268f3f54da8229b7057e0eaf836b
menganeP40 <menganeprashant40@gmail.com>
2025-06-09 10:33:08 +0530

    added some description of datastruture and algorithm

--- find_student.js
+++ find_student.js
@@ -1,5 +1,7 @@
+//this is the data structure we used (array)
 const studentDatabase = ['prashant','erick','amey','rishika'];
 
+//this is the algorithm we used to find the answer
 const findStudent = (allStudent , student_name) => {
   for(let i=1; i<=allStudent.length; i++){
     if(allStudent[i] === student_name){
